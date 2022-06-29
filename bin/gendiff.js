7a6f75e3cc731d3dd461a1e0a1cd4c8812f46ad2
Mikhail Suvorov <mr.plaim@gmail.com>
2022-06-29 21:41:27 +0300

    Step 2. Modify help menu.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
