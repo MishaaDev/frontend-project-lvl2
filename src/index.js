393b3ea0b38897001db8aa57a897382c639caf16
Mikhail Suvorov <mr.plaim@gmail.com>
2022-07-04 01:10:07 +0300

    fix tests

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const getFormat = (filename) => path.extname(filename).slice(1);
 const getFullPath = (filename) => path.resolve(process.cwd(), filename);
 const getFileContent = (filename) => fs.readFileSync(getFullPath(filename), 'utf-8');
 
-const genDiff = (path1, path2, format) => {
+const genDiff = (path1, path2, format = 'stylish') => {
   if (fs.existsSync(path1) && fs.existsSync(path2)) {
     const obj1 = fileParse(getFileContent(path1), getFormat(path1));
     const obj2 = fileParse(getFileContent(path2), getFormat(path2));
